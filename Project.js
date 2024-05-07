e1e2a94de113cb39d9b14b2ed490806fda2893aa
hxofficial <hxofficialmail@gmail.com>
2024-05-07 13:03:58 -0700

    spin and reels created

--- Project.js
+++ Project.js
@@ -33,7 +33,7 @@ const deposit = () => {
 };
 
 const depositAmount = deposit();
-console.log(depositAmount);
+// console.log(depositAmount);
 
 const getNumberOfLines = () => {
   while (true) {
@@ -60,6 +60,28 @@ const getBet = (balance, lines) => {
     }
   }
 };
+
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+  const reels = [[], [], []];
+  for (let i = 0; i < COLS; i++) {
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
+};
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
