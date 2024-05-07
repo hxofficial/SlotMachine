553793a4af3cc71ad08623dd44349b3e856d441c
hxofficial <hxofficialmail@gmail.com>
2024-05-07 12:17:17 -0700

    number of lines determined

--- Project.js
+++ Project.js
@@ -14,3 +14,18 @@ const deposit = () => {
 };
 const depositAmount = deposit();
 console.log(depositAmount);
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3) : ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("invalid number of lines,try again");
+    } else {
+      return numberOflines;
+    }
+  }
+};
+
+const numberOfLines = getNumberOfLines();
